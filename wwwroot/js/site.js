39fe7f7aee8bc34253cdcd85732b0e774cdfefc4
canue <canue@MSI>
2022-02-04 09:39:18 +0100

    Ajoutez des fichiers projet.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+var typed3 = new Typed('.typed', {
+    strings: ["Passionnée par l'univers du web et dotée d'une grande curiosité, je suis en reconversion professionnelle et suis une formation de développeuse web et web mobile.",
+        "J'aime la programmation et les nouvelles technologies.",
+        "Je recherche actuellement une entreprise pour perfectionner mes connaissances et mes compétences au cours d'un stage d'un mois."],
+    typeSpeed: 30,
+    backSpeed: 0,
+    smartBackspace: true, // this is a default
+    loop: false
+});
